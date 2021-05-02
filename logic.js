fe41f4cbf46b71b003fb169c551c073cdf511aac
Mark-Platts <markplattstutoring@outlook.com>
2021-05-02 20:31:01 +0100

    various

--- logic.js
+++ logic.js
@@ -5,26 +5,130 @@ let halt = false;
 let currentInstruction = '';
 let currentOperation = '';
 let currentValue = '0001';
+let play = false;
+let playSpeed = 4000;
 
+
+//Gets the code at a given address and returns it as an object with keys: instr, op and val
+function receiveCode(addr) {
+    let hold = {};
+    let divName = 'data'+String(binToDec(addr));
+    let instruction = document.getElementById(divName).value;
+    hold['instr'] = instruction;
+    hold['op'] = instruction.slice(0,4);
+    hold['val'] = instruction.slice(4);
+    return(hold);
+}
 
 //fetch: justs makes it clear that the counterNumber address is being looked at
 function fetch() {
+    document.getElementById("negativeFlag").innerHTML = 0;
+    document.getElementById("zeroFlag").innerHTML = 0;
+    document.getElementById("overflowFlag").innerHTML = 0;
+    document.getElementById("cyclePart").innerHTML = 'F';
     highlightCurrentAddress();
+    step = 1;
 }
 
 //decode: updates the data ready for the operation to be executed
 function decode() {
     let divName = 'data'+String(counterNumber);
+    document.getElementById("cyclePart").innerHTML = 'D';
     currentInstruction = document.getElementById(divName).value;
     currentOperation = currentInstruction.slice(0,4);
     document.getElementById("instructionDecode").innerHTML = currentOperation;
     currentValue = currentInstruction.slice(4);
+    step = 2;
+    console.log('Current Op: '+currentOperation);
+    console.log('Current Val: '+currentValue);
+}
+
+//Execute: executes the current operation and turns the counter (each subOp is responsible for the counter turn, in case of jumps)
+function execute() {
+    document.getElementById("cyclePart").innerHTML = 'E';
+    switch(currentOperation) {
+        case "0001":
+            loadOP();
+            console.log('Doing: Load');
+            break;
+        case "0002":
+            storeOp()
+            console.log('Doing: Store');
+            break;
+        case "0003":
+            addOp()
+            console.log('Doing: Add');
+            break;
+        default:
+            counterNumber++;
+            break;
+    }
+    step = 0;
+}
+
+//resets the program
+function reset() {
+    if (play) {
+        playPause();
+    }
+    step = 0;
+    counterNumber = 0;
+    halt = false;
+    currentInstruction = '';
+    currentOperation = '';
+    currentValue = '';
+    document.getElementById("accumulator").innerHTML = '0000';
+    document.getElementById("instructionDecode").innerHTML = '0000';
+    document.getElementById("negativeFlag").innerHTML = 0;
+    document.getElementById("zeroFlag").innerHTML = 0;
+    document.getElementById("overflowFlag").innerHTML = 0;
+    document.getElementById("InstAddReg").innerHTML = '0000';
+    document.getElementById("cyclePart").innerHTML = 'S';
+    resetHighlights();
+}
+
+//Completes the current step
+function doStep() {
+    if (halt) {
+        return
+    }
+    if (step == 0) {
+        fetch();
+    }
+    else if (step == 1) {
+        decode();
+    }
+    else if (step == 2) {
+        execute();
+    }
+    updateCounter();
+}
+
+//In case of halt, checks if play is true
+function haltCheck() {
+    if (play) {
+        clearInterval(playTimer);
+    }
+}
+
+//starts or stops the program at the chosen speed
+function playPause() {
+    if (play) {
+        clearInterval(playTimer);
+        play = false;
+    }
+    else {
+        getPlaySpeed();
+        playTimer = setInterval(doStep, playSpeed);
+        play = true;
+    }
 }
 
 
 //opCode: 0001, loads value from address into acc
 function loadOP() {
     document.getElementById("accumulator").innerHTML = currentValue;
+    counterNumber++;
 }
 
 //opCode: 0002, stores acc value at the address
@@ -32,16 +136,20 @@ function storeOp() {
     let addressDecNumber = binToDec(currentValue);
     let divName = 'data'+String(addressDecNumber);
     document.getElementById(divName).value = '0000'+document.getElementById("accumulator").innerHTML;
+    counterNumber++;
 }
 
 //opCode: 0003, Adds the value at the address to the acc
 function addOp() {
     let acc = binToDec(document.getElementById("accumulator").innerHTML);
     let add = binToDec(currentValue);
-    let fin = acc + add;
+    let val = document.getElementById('data'+String(add)).value;
+    let num = binToDec(val.slice(4));
+    let fin = acc + num;
     if (fin > 15) {
         fin -= 15;
         document.getElementById("overflowFlag").innerHTML = 1;
     }
     document.getElementById("accumulator").innerHTML = decToBin(fin);
+    counterNumber++;
 }
--- ui.js
+++ ui.js
@@ -8,7 +8,7 @@ function saveState() {
 
 //Highlights current address from counter
 function highlightCurrentAddress() {
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < 16; i++) {
         if (i == counterNumber) {
             document.getElementById('row'+String(i)).className = 'counterTurn';
         }
@@ -18,7 +18,126 @@ function highlightCurrentAddress() {
     }
 }
 
+//Highlights current address if an error occurs
+function highlightError() {
+    for (let i = 0; i < 16; i++) {
+        if (i == counterNumber) {
+            document.getElementById('row'+String(i)).className = 'errorHighlight';
+        }
+        else {
+            document.getElementById('row'+String(i)).className = '';
+        }
+    }
+}
 
+//resets all highlights
+function resetHighlights() {
+    for (let i = 0; i < 16; i++) {
+        document.getElementById('row'+String(i)).className = '';
+    }
+}
+
+//updates the counter with a binary number
+function updateCounter() {
+    document.getElementById("InstAddReg").innerHTML = decToBin(counterNumber);
+}
+
+//gets the currently selected play speed
+function getPlaySpeed() {
+    if (document.getElementById("0.25hz").checked) {
+        playSpeed = 4000;
+    }
+    else if (document.getElementById("1hz").checked) {
+        playSpeed = 1000;
+    }
+    else if (document.getElementById("10hz").checked) {
+        playSpeed = 100;
+    }
+}
+
+
+//Checks if a string only contains 0 or 1, returns bool
+function checkIfBin(str) {
+    let ans = true;
+    for (let i=0; i < str.length; i++) {
+        let test = str.charAt(i);
+        if (test != '0' && test != '1') {
+            ans = false;
+        }
+    }
+    return(ans);
+}
+
+//Takes an address and translates its code
+function translate(addr) {
+    let code = receiveCode(addr);
+    if (checkIfBin(code.instr) != true) {
+        return('INP ERR')
+    }
+    else if (code.instr.length != 8) {
+        return('LEN ERR');
+    }
+    else {
+        let num = binToDec(code.val);
+        let opCode = opTrans[code.op];
+        return(opCode + ' ' + String(num));
+    }
+}
+
+//Goes through each RAM address and updates the translation
+function updateTrans() {
+    for (let i=0; i<16; i++) {
+        let addr = decToBin(i);
+        document.getElementById('dec'+i).innerHTML = translate(addr);
+    }
+}
+
+//dictionary for translation of ops
+const opTrans = {
+    "0000": "NUL",
+    "0001": "CLR",
+    "0010": "HLT",
+    "0011": "ERR",
+    "0100": "LOD",
+    "0101": "SAV",
+    "0110": "ADD",
+    "0111": "SUB",
+    "1000": "JMP",
+    "1001": "JIN",
+    "1010": "JIZ",
+    "1011": "JOI",
+    "1100": "ERR",
+    "1101": "ERR",
+    "1110": "ERR",
+    "1111": "ERR"
+};
+
+//updates the flags to the given arguments
+function updateFlags(n,z,o) {
+    document.getElementById("negativeFlag").innerHTML = n;
+    document.getElementById("zeroFlag").innerHTML = z;
+    document.getElementById("overflowFlag").innerHTML = o;
+}
+
+
+
+
+//Turns the RAM data into one string and saves to clipboard
+// function saveToClip() {
+//     let saveData = '';
+//     for (let i=0; i < 15; i++) {
+//         saveData += document.getElementById('data'+String(counterNumber)).value;
+//     }
+//     /* Select the text field */
+//     saveData.select();
+//     saveData.setSelectionRange(0, 99999); /* For mobile devices */
+
+//     /* Copy the text inside the text field */
+//     document.execCommand("copy");
+    
+//     /* Alert the copied text */
+//     alert("Copied the text: " + saveData.value);
+// }
 
 //Makes a div blink
 function Ablink(divID) {
